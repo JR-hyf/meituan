c948ed7090808bc1d24c7e12ab03b9ef36f81190
Bill <Eldred@wfefrg.local>
2016-06-13 14:34:41 +0800

    创建一个狗类

---
+++ meituan/meituan/dog.h
@@ -0,0 +1,15 @@
+//
+//  dog.h
+//  meituan
+//
+//  Created by Bill on 13/6/16.
+//  Copyright © 2016年 my xcode01. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+
+@interface dog : NSObject
+
+@property(nonatomic,assign)int age;
+
+@end
---
+++ meituan/meituan/dog.m
@@ -0,0 +1,13 @@
+//
+//  dog.m
+//  meituan
+//
+//  Created by Bill on 13/6/16.
+//  Copyright © 2016年 my xcode01. All rights reserved.
+//
+
+#import "dog.h"
+
+@implementation dog
+
+@end
