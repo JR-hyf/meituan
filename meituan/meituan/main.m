d1833cc35b7fe2d1c53c3a6a1891fc6fd197588b
Bill <Eldred@wfefrg.local>
2016-06-13 14:28:52 +0800

    lalala

---
+++ meituan/meituan/main.m
@@ -0,0 +1,16 @@
+//
+//  main.m
+//  meituan
+//
+//  Created by Bill on 13/6/16.
+//  Copyright © 2016年 my xcode01. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+#import "AppDelegate.h"
+
+int main(int argc, char * argv[]) {
+    @autoreleasepool {
+        return UIApplicationMain(argc, argv, nil, NSStringFromClass([AppDelegate class]));
+    }
+}
